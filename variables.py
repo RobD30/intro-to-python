18b7df44ce5be22ad015310b311f0dea21d4456c
Robert Dickerson <rdickeriii@gmail.com>
2017-07-14 19:05:40 -0700

    Refactor: fixed math
    end_of_line

--- variables.py
+++ variables.py
@@ -5,17 +5,17 @@ reservoir_volume = 4.445e8
 rainfall = 5e6
 
 # decrease the rainfall variable by 10% to account for runoff
-rainfall = rainfall - (rainfall *= 0.1)
+rainfall *= 0.1
 
 # add the rainfall variable to the reservoir_volume variable
-rainfall + reservoir_volume
+reservoir_volume += rainfall
 
 # increase reservoir_volume by 5% to account for stormwater that flows
 # into the reservoir in the days following the storm
-reservoir_volume = reservoir_volume + (reservoir_volume *= 0.05)
+reservoir_volume *= 0.05
 
 # decrease reservoir_volume by 5% to account for evaporation
-reservoir_volume = reservoir_volume + (reservoir_volume *= 0.05)
+reservoir_volume *= 0.05
 
 # subtract 2.5e5 cubic metres from reservoir_volume to account for water
 # that's piped to arid regions.
