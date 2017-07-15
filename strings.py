d2207ade00f043a0bf1ccb4055b3545882470e8e
Robert Dickerson <rdickeriii@gmail.com>
2017-07-14 23:11:12 -0700

    Refactor: Wrote "working" code.
    end_of_line

---
+++ strings.py
@@ -0,0 +1,12 @@
+prophecy = "And there shall in that time be rumours of things going astray, and there will be a great confusion as to where things really are, and nobody will really know where lieth those little things with the sort of raffia work base, that has an attachment…at this time, a friend shall lose his friends’s hammer and the young shall not know where lieth the things possessed by their fathers that their fathers put there only just the night before around eight o’clock…"
+
+vowel_count = prophecy.count('a,e,i,o,u')
+
+# TODO: set the value of vowel_count to be the number of vowels in prophecy
+
+
+
+# Print the final count
+print(vowel_count)
+
+Today's forecast for Seoul: The temperature will range from 9 to 18 degrees Celsius. Conditions will be light rain.
