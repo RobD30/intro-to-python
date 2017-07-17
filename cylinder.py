4f53b16577065c903677885534e882721283002f
Robert Dickerson <rdickeriii@gmail.com>
2017-07-16 18:34:28 -0700

    Refactor: Changed code
    end_of_line

---
+++ cylinder.py
@@ -0,0 +1,8 @@
+def cylinder_volume(heigt, radius):
+    pi = 3.14159
+    return heigt * pi * (radius ** 2)
+
+
+def population_density(population, land_area):
+    density = (population / land_area)
+    return (density)
