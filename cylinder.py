2c133386afb339613c90c392994a6a4c44d74006
Robert Dickerson <rdickeriii@gmail.com>
2017-07-16 18:35:49 -0700

    Chore: Renaming
    end_of_line

--- cylinder.py
+++
@@ -1,8 +0,0 @@
-def cylinder_volume(heigt, radius):
-    pi = 3.14159
-    return heigt * pi * (radius ** 2)
-
-
-def population_density(population, land_area):
-    density = (population / land_area)
-    return (density)
