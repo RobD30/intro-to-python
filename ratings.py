6c0909a4b8b67d6bb59ea8b881994a21fd6293c4
Robert Dickerson <rdickeriii@gmail.com>
2017-07-17 17:14:42 -0700

    Refactor: Created several functions.
    end_of_line

--- ratings.py
+++ ratings.py
@@ -1,20 +1,42 @@
-def scores_to_rate(score1,score2,score3,score4,score5):
+def convert_to_numeric(score):
+    """
+    Converts the scores into a numerical value
+    """
+    return float(score)
+
+
+def sum_of_middle_three(score1, score2, score3, score4, score5):
+    """
+    Gives the sum
+    """
+    sum =
+    return
+
+
+def score_to_rating_string(score):
+    """
+
+    :return:
+    """
+    rating =
+    return rating
+
+
+def scores_to_rate(score1, score2, score3, score4, score5):
     """
     Turns 5 scores into a rating by averaging the middle 3.
     """
-    #TODO convert scores into numbers
-    score1 = convert_tonumeric(score1)
-    score2 = convert_tonumeric(score2)
-    score3 = convert_tonumeric(score3)
-    score4 = convert_tonumeric(score4)
-    score5 = convert_tonumeric(score5)
+    # TODO convert scores into numbers
+    score1 = convert_to_numeric(score1)
+    score2 = convert_to_numeric(score2)
+    score3 = convert_to_numeric(score3)
+    score4 = convert_to_numeric(score4)
+    score5 = convert_to_numeric(score5)
 
-    #TODO: Find the middle three scores
+    # TODO: combined steps
+    average_score = sum_of_middle_three(score1, score2, score3, score4, score5) / 3
 
-    #TODO: Take average of middle three scores
-    average_score = #((score1 + score2 + score3) / 3)
-
-    #TODO: Turn average score into a rating
+    # TODO: Turn average score into a rating
     rating = score_to_rating_string(average_score)
 
     return rating
