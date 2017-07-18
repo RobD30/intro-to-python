8b039b677fe6beebcedbc1204e5a57ef2becfc89
Robert Dickerson <rdickeriii@gmail.com>
2017-07-18 16:51:57 -0700

    Refactor: Spam!
    end_of_line

--- lists.py
+++ lists.py
@@ -1,3 +1,12 @@
 def foo():
-    list = [1, 2, 3, 4, 5, 6, 7, 8]
-    return list
+    order = [1, 2, 3, 4, 5, 6, 7, 8]
+    return order
+
+
+dish = ["Spam", "Spam", "Spam", "Spam", "Spam", "Spam", "baked beans", "Spam", "Spam", "Spam", "Spam"]
+mr_buns_order = dish
+print(dish)
+print(mr_buns_order)
+dish[6] = "Spam"  # baked beans are off
+print(mr_buns_order)
+print(dish)
