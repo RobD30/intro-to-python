5db48c56ed602cbd607575d176ae25fc56bd20a2
Robert Dickerson <rdickeriii@gmail.com>
2017-07-18 17:24:48 -0700

    Refactor: Can't seem to get this right.
    end_of_line

--- lists.py
+++ lists.py
@@ -10,3 +10,13 @@ print(mr_buns_order)
 dish[6] = "Spam"  # baked beans are off
 print(mr_buns_order)
 print(dish)
+
+
+def top_three(input_list):
+    """Returns a list of the three largest elements input_list in order from largest to smallest.
+
+    If input_list has fewer than three elements, return input_list element sorted largest to smallest/
+    """
+    # TODO: implement this function
+
+    return sorted(input_list[-3:0])
